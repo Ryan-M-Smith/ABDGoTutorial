ca127b943f506f089c1bf3c376c45a166b4684ce
Ryan Smith <rysmith2113@gmail.com>
2025-11-12 17:30:49 -0500

    first commit

---
+++ exercise01.go
@@ -0,0 +1,15 @@
+//
+// Filename: exercise01.go | Alpha Beta Debug Go Tutorial
+// Description: Hello World program
+// Copyright (c) 2025 Ryan Smith
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
